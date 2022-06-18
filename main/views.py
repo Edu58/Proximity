0d9e4ed836c1737e1a37eed27de0ce6d14711830
“Edu58” <edumuriithi58@gmail.com>
2022-06-18 13:23:34 +0300

    adds signup view

--- main/views.py
+++ main/views.py
@@ -1,5 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import SignUpForm
+from django.contrib import messages
 
 
 def index(request):
     return render(request, 'index.html')
+
+
+def signup_user(request):
+    form = SignUpForm()
+
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Account created successfully')
+            return redirect('login')
+
+        messages.warning(request, 'Please provide the required credentials!')
+    return render(request, 'signup.html', {'form': form})
