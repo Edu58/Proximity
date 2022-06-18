918d5a3b6a169feecd12fdf94c373e94c1949fe5
“Edu58” <edumuriithi58@gmail.com>
2022-06-18 11:32:52 +0300

    registers models

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Profile, Event, Announcement, Alert
 
-# Register your models here.
+
+admin.site.register([
+    Profile,
+    Event,
+    Announcement,
+    Alert
+])
