d12fe1c3ad70e2894b4df96433d3814b058cc00f
“Edu58” <edumuriithi58@gmail.com>
2022-06-18 13:11:40 +0300

    style: index.html

---
+++ static/js/base.js
@@ -0,0 +1,27 @@
+$('document').ready(function () {
+    // Sticky header on scroll
+    const selectHeader = document.querySelector('#header');
+    if (selectHeader) {
+        document.addEventListener('scroll', () => {
+            window.scrollY > 100 ? selectHeader.classList.add('sticked') : selectHeader.classList.remove('sticked');
+        });
+    }
+
+    // Mobile nav toggle
+
+    const mobileNavShow = document.querySelector('.mobile-nav-show');
+    const mobileNavHide = document.querySelector('.mobile-nav-hide');
+
+    document.querySelectorAll('.mobile-nav-toggle').forEach(el => {
+        el.addEventListener('click', function (event) {
+            event.preventDefault();
+            mobileNavToogle();
+        })
+    });
+
+    function mobileNavToogle() {
+        document.querySelector('body').classList.toggle('mobile-nav-active');
+        mobileNavShow.classList.toggle('d-none');
+        mobileNavHide.classList.toggle('d-none');
+    }
+})
