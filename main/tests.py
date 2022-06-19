96600ffbdc136c1a9d83d0c2e2b6ad27cfa85cbc
“Edu58” <edumuriithi58@gmail.com>
2022-06-19 19:33:56 +0300

    adds TestPost testcase

--- main/tests.py
+++ main/tests.py
@@ -92,7 +92,6 @@ from .models import Business, Neighbourhood, Profile, Post, Category
 #         all_neighbourhoods = Neighbourhood.objects.all()
 #         self.assertTrue(len(all_neighbourhoods) == 0)
 
-
 # class TestBusiness(TestCase):
 
 #     def setUp(self):
@@ -148,14 +147,69 @@ from .models import Business, Neighbourhood, Profile, Post, Category
 #         all_businesses = Business.objects.all()
 #         self.assertTrue(len(all_businesses) == 0)
 
+# class TestCategory(TestCase):
 
-class TestCategory(TestCase):
+#     def setUp(self):
+#         self.new_category = Category(
+#             name="test category"
+#         )
+#         self.new_category.save()
+
+#     def tearDown(self):
+#         User.objects.all().delete()
+#         Profile.objects.all().delete()
+#         Neighbourhood.objects.all().delete()
+#         Business.objects.all().delete()
+#         Post.objects.all().delete()
+
+#     def test_category_instance(self):
+#         self.assertTrue(isinstance(self.new_category, Category))
+
+#     def test_add_category(self):
+#         self.another_category = Category(name="another test category")
+#         self.another_category.save_category()
+
+#         all_categories = Category.objects.all()
+#         self.assertTrue(len(all_categories) > 1)
+
+#     def test_delete_category(self):
+#         self.new_category.delete_category()
+
+#         all_categories = Category.objects.all()
+#         self.assertTrue(len(all_categories) == 0)
+
+
+class TestPost(TestCase):
 
     def setUp(self):
-        self.new_category = Category(
-            name="test category"
-        )
+        self.john = User(username="john",
+                         first_name="john",
+                         last_name="bravo",
+                         email="john@gmail.com",
+                         password="john98765")
+        self.john.save()
+
+        self.new_category = Category(name="test category")
         self.new_category.save()
+
+        self.neighbourhood = Neighbourhood(name="sabina",
+                                           location="nrb",
+                                           number_of_occupants=3456,
+                                           health_dept_contact=75345345,
+                                           fire_dept_contact=74534534,
+                                           police_dept_contact=7435345,
+                                           admin=self.john)
+
+        self.neighbourhood.save()
+
+        self.new_post = Post(
+            user=self.john,
+            category=self.new_category,
+            neighbourhood=self.neighbourhood,
+            title='a test post',
+            description='This description is just for testing')
+
+        self.new_post.save()
 
     def tearDown(self):
         User.objects.all().delete()
@@ -164,18 +218,24 @@ class TestCategory(TestCase):
         Business.objects.all().delete()
         Post.objects.all().delete()
 
-    def test_category_instance(self):
-        self.assertTrue(isinstance(self.new_category, Category))
+    def test_post_instance(self):
+        self.assertTrue(isinstance(self.new_post, Post))
 
-    def test_add_category(self):
-        self.another_category = Category(name="another test category")
-        self.another_category.save_category()
+    def test_add_post(self):
+        self.another_post = Post(
+            user=self.john,
+            category=self.new_category,
+            neighbourhood=self.neighbourhood,
+            title='another test post',
+            description='This description is just for testing and it is the second one.')
 
-        all_categories = Category.objects.all()
-        self.assertTrue(len(all_categories) > 1)
+        self.another_post.save_post()
 
-    def test_delete_category(self):
-        self.new_category.delete_category()
+        all_posts = Post.objects.all()
+        self.assertTrue(len(all_posts) > 1)
 
-        all_categories = Category.objects.all()
-        self.assertTrue(len(all_categories) == 0)
+    def test_delete_business(self):
+        self.new_post.delete_post()
+
+        all_posts = Post.objects.all()
+        self.assertTrue(len(all_posts) == 0)
