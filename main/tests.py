bb970793551ed8e2eade7673a058ac3c166ed1ab
“Edu58” <edumuriithi58@gmail.com>
2022-06-19 19:15:59 +0300

    add TestNeighbourhood testcase

--- main/tests.py
+++ main/tests.py
@@ -24,8 +24,11 @@ class TestProfile(TestCase):
         self.neighbourhood.save()
 
     def tearDown(self):
-        self.john.delete()
-        self.neighbourhood.delete()
+        User.objects.all().delete()
+        Profile.objects.all().delete()
+        Neighbourhood.objects.all().delete()
+        Business.objects.all().delete()
+        Post.objects.all().delete()
 
     def test_user_instance(self):
         self.assertTrue(isinstance(self.john, User))
@@ -39,5 +42,55 @@ class TestProfile(TestCase):
         )
 
         self.assertTrue(isinstance(self.john_profile, Profile))
+
+
+class TestNeighbourhood(TestCase):
+
+    def setUp(self):
+        self.john = User(username="john",
+                         first_name="john",
+                         last_name="bravo",
+                         email="john@gmail.com",
+                         password="john98765")
+        self.john.save()
+
+        self.neighbourhood = Neighbourhood(name="sabina",
+                                           location="nrb",
+                                           number_of_occupants=3456,
+                                           health_dept_contact=75345345,
+                                           fire_dept_contact=74534534,
+                                           police_dept_contact=7435345,
+                                           admin=self.john)
+
+        self.neighbourhood.save()
+
+    def tearDown(self):
+        User.objects.all().delete()
+        Profile.objects.all().delete()
+        Neighbourhood.objects.all().delete()
+        Business.objects.all().delete()
+        Post.objects.all().delete()
+
+    def test_neighbourhood_instance(self):
+        self.assertTrue(isinstance(self.neighbourhood, Neighbourhood))
+
+    def test_add_neighbourhood(self):
+        self.test_neighbourhood = Neighbourhood(name="cow",
+                                                location="kakamega",
+                                                number_of_occupants=34656,
+                                                health_dept_contact=79345345,
+                                                fire_dept_contact=74354534,
+                                                police_dept_contact=74307345,
+                                                admin=self.john)
+        self.test_neighbourhood.create_neighbourhood()
+
+        all_neighbourhoods = Neighbourhood.objects.all()
+        self.assertTrue(len(all_neighbourhoods) > 0)
+
+    def test_delete_neighbourhood(self):
+        self.neighbourhood.delete_neighbourhood()
+
+        all_neighbourhoods = Neighbourhood.objects.all()
+        self.assertTrue(len(all_neighbourhoods) == 0)
 
 
